ffbf84030618e645055a03786c8079f8594c723c
[satuhalinen] <satuh9@gmail.com>
2024-02-11 21:14:39 +0200

    add addition also for other numbers than 7

--- src/calculator.ts
+++ src/calculator.ts
@@ -12,10 +12,14 @@ function inputOperator(operator: string) {
     displayInput.value = operator;
   }
 }
-const sevenButton = document.getElementById("sevenButton") as HTMLButtonElement;
 
-if (sevenButton) {
-  sevenButton.addEventListener("click", () => inputNumber("7"));
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+for (let number of numbers) {
+  const button = document.getElementById(number + 'Button') as HTMLButtonElement;
+  if (button) {
+    button.addEventListener("click", () => inputNumber(number));
+  }
 }
 
 const plusButton = document.getElementById("plusButton") as HTMLButtonElement;
